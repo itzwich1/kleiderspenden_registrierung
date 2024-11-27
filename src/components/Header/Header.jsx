2262ecc187fa38cfaad838192246ec20379abe64
Felix Wich <fx.wich@outlook.de>
2024-11-27 18:50:14 +0100

    Navbar angefangen

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import {Link} from "react-router-dom"
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/ueberuns">Über uns</Link></li>
+          <li><Link to="/unseregeschichte">Unsere Geschichte</Link></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createRoot } from 'react-dom/client';
 
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);

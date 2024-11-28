850dd490d856f76275c2fea8776f4a2592e36d1f
Felix Wich <fx.wich@outlook.de>
2024-11-28 14:44:36 +0100

    Header fix zu sehen. Zwischen Seiten hin und her switchen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Home from "./pages/home";
+import UeberUns from "./pages/ueberuns";
+import UnsereGeschichte from "./pages/unseregeschichte";
 
 /*import Home from "./pages/Home";
 import UeberUns from "./pages/UeberUns";
@@ -10,9 +13,14 @@ import UnsereGeschichte from "./pages/UnsereGeschichte";*/
 export default function App() {
   return (
     <div>
+      
       <BrowserRouter>
+      <Header />
         <Routes>
-          <Route index element={<Header />} />
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/UeberUns" element={<UeberUns />} />
+          <Route path="/UnsereGeschichte" element={<UnsereGeschichte />} />
         </Routes>
       </BrowserRouter>
     </div>
